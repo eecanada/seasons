50ecad76e8cdcf79927a22f13ea739c29ba9b981
Eder Canada <canada.eder@gmail.com>
2021-10-11 12:02:25 -0700

    season logic

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: '' };
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: 'location cannot be found' });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: 'location cannot be found' })
     );
   }
+
+  componentDidUpdate() {
+    console.log('component was just updated - it rerendered');
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div> Loading!</div>;
   }
